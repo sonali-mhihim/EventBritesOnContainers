b251a5470fd7d494d9258e10fe4d3848dd5daaeb
Soo Kim <hangbokhe@users.noreply.github.com>
2020-06-09 00:15:27 -0700

    Catalog Initial Set-up

---
+++ EventCatalogAPI/Data/CatalogContext.cs
@@ -0,0 +1,47 @@
+﻿using EventCatalogAPI.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EventCatalogAPI.Data
+{
+    public class CatalogContext : DbContext
+    {
+        //server constructor for dependency injection
+        public CatalogContext(DbContextOptions options) : base (options)
+        {
+
+        }
+        public DbSet<CatalogEventType> CatalogEventTypes { get; set; }
+        public DbSet<CatalogHost> CatalogHosts { get; set; }
+        public DbSet<CatalogEvent> CatalogEvents { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<CatalogEvent>(e =>
+            {
+                e.ToTable("CatalogEvents");
+                e.Property(b => b.Id).IsRequired().UseHiLo("catalog_event_hilo");
+                e.Property(b => b.Name).IsRequired().HasMaxLength(100);
+                e.Property(b => b.Price).IsRequired();
+                e.HasOne(b => b.EventHost).WithMany().HasForeignKey(c => c.CatalogHostId);
+                e.HasOne(b => b.EventType).WithMany().HasForeignKey(c => c.CatalogTypeId);
+            });
+
+            modelBuilder.Entity<CatalogEventType>(e =>
+            {
+                e.ToTable("CatalogEventTypes");
+                e.Property(b => b.Id).IsRequired().UseHiLo("catalog_types_hilo");
+                e.Property(b => b.Name).IsRequired().HasMaxLength(100);
+            });
+            modelBuilder.Entity<CatalogHost>(e =>
+            {
+                e.ToTable("CatalogEventHosts");
+                e.Property(b => b.Id).IsRequired().UseHiLo("catalog_hosts_hilo");
+                e.Property(b => b.Name).IsRequired().HasMaxLength(100);
+            });
+        }
+    }
+}
---
+++ EventCatalogAPI/Domain/CatalogEvent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using static EventCatalogAPI.Domain.CatalogHost;
+
+namespace EventCatalogAPI.Domain
+{
+    public class CatalogEvent
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string PictureUrl { get; set; }
+        public int CatalogHostId { get; set; }
+        public int CatalogTypeId { get; set; }
+        public CatalogHost EventHost { get; set; }
+        public CatalogEventType EventType { get; set; }
+
+    }
+}
