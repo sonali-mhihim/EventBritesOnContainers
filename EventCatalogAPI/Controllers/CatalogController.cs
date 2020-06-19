c19fdea95376820529a63e816c5c3e4323bd739e
sonali-mhihim <63938232+sonali-mhihim@users.noreply.github.com>
2020-06-19 16:14:37 -0700

    Add controllers

---
+++ EventCatalogAPI/Controllers/CatalogController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EventCatalogAPI.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EventCatalogAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CatalogController : ControllerBase
+    {
+        private readonly CatalogContext _context;
+
+        public CatalogController(CatalogContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("[action]")]
+        public async Task<IActionResult> Events(
+            [FromQuery]int pageIndex = 0,
+            [FromQuery]int pagesize = 3)
+        { var events = await _context.CatalogEvents
+                .OrderBy(c => c.Name)
+                .Skip(pageIndex * pagesize)
+                .Take(pagesize)
+                .ToListAsync();
+
+            return Ok(events);
+        }
+    }
+}
