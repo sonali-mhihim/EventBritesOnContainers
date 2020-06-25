2fe784c1237ca12d45d8c250866e677b4315e868
Soo Kim <hangbokhe@users.noreply.github.com>
2020-06-24 18:41:50 -0700

    ViewModels Updated

--- EventCatalogAPI/Controllers/CatalogController.cs
+++ EventCatalogAPI/Controllers/CatalogController.cs
@@ -3,9 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using EventCatalogAPI.Data;
+using EventCatalogAPI.Domain;
+using EventCatalogAPI.ViewModels;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Features;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace EventCatalogAPI.Controllers
 {
@@ -14,23 +18,39 @@ namespace EventCatalogAPI.Controllers
     public class CatalogController : ControllerBase
     {
         private readonly CatalogContext _context;
+        private readonly IConfiguration _config;
 
-        public CatalogController(CatalogContext context)
+        public CatalogController(CatalogContext context, IConfiguration config)
         {
             _context = context;
+            _config = config;
         }
 
         [HttpGet("[action]")]
-        public async Task<IActionResult> Events(
-            [FromQuery]int pageIndex = 0,
-            [FromQuery]int pagesize = 3)
-        { var events = await _context.CatalogEvents
+        public async Task<IActionResult> Events([FromQuery]int pageIndex = 0,[FromQuery]int pagesize = 3)
+        {
+            var eventsCount = await _context.CatalogEvents.LongCountAsync();
+            var events = await _context.CatalogEvents
                 .OrderBy(c => c.Name)
                 .Skip(pageIndex * pagesize)
                 .Take(pagesize)
                 .ToListAsync();
 
-            return Ok(events);
+            events = ChangePictureUrl(events);
+            var model = new PaginatedEventsViewModel<CatalogEvent>
+            {
+                PageIndex = pageIndex,
+                PageSize = events.Count,
+                Count = eventsCount,
+                Data = events
+            };
+            return Ok(model);
+        }
+
+        private List<CatalogEvent> ChangePictureUrl(List<CatalogEvent> events)
+        {
+            events.ForEach(events => events.PictureUrl = events.PictureUrl.Replace("http://externalcatalogbaseurltobereplaced", _config["ExternalCatalogBaseUrl"]));
+            return events;
         }
     }
 }
---
+++ EventCatalogAPI/ViewModels/PaginatedEventsViewModel.cs
@@ -0,0 +1,16 @@
+﻿using EventCatalogAPI.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EventCatalogAPI.ViewModels
+{
+    public class PaginatedEventsViewModel<T>
+    {
+        public int PageIndex { get; set; }
+        public int PageSize { get; set; }
+        public long Count { get; set; }
+        public IEnumerable<T> Data { get; set; }
+    }
+}
