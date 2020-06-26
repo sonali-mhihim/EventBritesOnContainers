77569616fdf2b941e616154585dc4931c43e33d8
Ana GOMEZ-MAYEN <31400695+anagomezmayen@users.noreply.github.com>
2020-06-26 16:30:35 -0700

    New APIs to filter the events by type and host.

--- EventCatalogAPI/Controllers/CatalogController.cs
+++ EventCatalogAPI/Controllers/CatalogController.cs
@@ -49,8 +49,65 @@ namespace EventCatalogAPI.Controllers
 
         private List<CatalogEvent> ChangePictureUrl(List<CatalogEvent> events)
         {
-            events.ForEach(events => events.PictureUrl = events.PictureUrl.Replace("http://externalcatalogbaseurltobereplaced", _config["ExternalCatalogBaseUrl"]));
+            events.ForEach(events => events.PictureUrl = events.PictureUrl.Replace("http://externalcatalogbaseurltobereplaced/", _config["ExternalCatalogBaseUrl"]));
             return events;
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        public async Task<IActionResult> CatalogEventTypes()
+        {
+            var types = await _context.CatalogEventTypes.ToListAsync();
+            return Ok(types);
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        public async Task<IActionResult> CatalogHosts()
+        {
+            var hosts = await _context.CatalogHosts.ToListAsync();
+            return Ok(hosts);
+        }
+
+        [HttpGet("[action]/type/{catalogTypeId}/host/{catalogHostId}")]
+        public async Task<IActionResult> Events(
+            int? catalogTypeId,
+            int? catalogHostId,
+            [FromQuery] int pageIndex = 0,
+            [FromQuery] int pageSize = 6
+            )
+        {
+            var query = (IQueryable<CatalogEvent>)_context.CatalogEvents;
+
+            if (catalogTypeId.HasValue)
+            {
+                query = query.Where(c => c.CatalogTypeId == catalogTypeId);
+            }
+
+            if (catalogHostId.HasValue)
+            {
+                query = query.Where(c => c.CatalogHostId == catalogHostId);
+            }
+
+            var events = await query
+                            .OrderBy(c => c.Date)
+                            .Skip(pageIndex * pageSize)
+                            .Take(pageSize)
+                            .ToListAsync();
+
+            var eventsCount = query.LongCountAsync();
+
+            events = ChangePictureUrl(events);
+
+            var model = new PaginatedEventsViewModel<CatalogEvent>
+            {
+                PageIndex = pageIndex,
+                PageSize = events.Count,
+                Count = eventsCount.Result,
+                Data = events
+            };
+
+            return Ok(model);
         }
     }
 }
