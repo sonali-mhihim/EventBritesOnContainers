c274955cd1187553db5a9520e86459f89048f671
Ana GOMEZ-MAYEN <31400695+anagomezmayen@users.noreply.github.com>
2020-07-24 14:23:00 -0700

    CartApi

---
+++ CartApi/Models/ICartRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CartApi.Models
+{
+    interface ICartRepository
+    {
+        Task<Cart> GetCartAsync(string cartId);
+        Task<Cart> UpdateCartAsync(Cart basket);
+        Task<bool> DeleteCartAsync(string id);
+        IEnumerable<string> GetUsers();
+    }
+}
